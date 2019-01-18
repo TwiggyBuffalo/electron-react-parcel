9ba1ff547123061e9d110f0e73e22e0f813766d7
Daniel Curran <dcurran@fullfacing.com>
2019-01-19 00:51:47 +0200

    init :tada:

---
+++ electron-start.js
@@ -0,0 +1,66 @@
+const {
+  app,
+  BrowserWindow,
+  systemPreferences,
+} = require('electron')
+const reload = require('electron-reload')
+const path = require('path')
+const url = require('url')
+const setupEvents = require('./installers/setupEvents')
+
+if (setupEvents.handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return
+}
+
+if (process.env.ELECTRON_START_URL) {
+  reload(__dirname)
+}
+
+let mainWindow
+
+function mainWindowClosed() {
+  mainWindow = null
+}
+
+function allWindowsClosed() {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+}
+
+function createWindow() {
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, './src/index.html'),
+    protocol: 'file:',
+    slashes: true,
+  })
+  const vibrancy = systemPreferences.isDarkMode() ? 'ultra-dark' : 'light'
+  const mainWindowConfig = {
+    vibrancy,
+    title: 'Epic Electron',
+  }
+  // Create the browser window.
+  mainWindow = new BrowserWindow(mainWindowConfig)
+  mainWindow.setFullScreen(true)
+  mainWindow.loadURL(startUrl)
+
+  mainWindow.loadURL('./src/index.html')
+
+  mainWindow.on('closed', mainWindowClosed)
+}
+
+function appActivated() {
+  if (mainWindow === null) {
+    createWindow()
+  }
+}
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', allWindowsClosed)
+
+app.on('activate', appActivated)
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { render } from 'react-dom'
+import styles from './index.css'
+import logoReact from './react.svg'
+import logoElectron from './electron.svg'
+
+const App = () => (
+  <div className={styles.app}>
+    <header className={styles.header}>
+      <div className={styles.logos}>
+        <img src={logoReact} className={styles.logo} alt="react" />
+        <img src={logoElectron} className={styles.logo} alt="electron" />
+      </div>
+      <p>
+        <span>Edit </span>
+        <code>src/index.js</code>
+        <span> and save to reload.</span>
+      </p>
+      <div className={styles.buttons}>
+        <a
+          className={styles.btnReact}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className={styles.btnElectron}
+          href="https://electronjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Electron
+        </a>
+      </div>
+    </header>
+  </div>
+)
+
+render(<App />, document.getElementById('root'))
